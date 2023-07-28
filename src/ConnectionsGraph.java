c1c3ea703c51d55f0f8b2ed3824ceb4ac9527e29
Shioxri <troybelardo@gmail.com>
2023-07-28 13:39:46 +0800

    "fixed" duplicate friends and DFS -> BFS

--- src/ConnectionsGraph.java
+++ src/ConnectionsGraph.java
@@ -4,27 +4,30 @@ import java.util.HashSet;
 
 public class ConnectionsGraph {
     private int numAccounts;
-    private HashMap<Integer, ArrayList<Integer>> connectionsList;
+    private HashMap<Integer, HashSet<Integer>> connectionsList;
     public ConnectionsGraph(int numAccounts) {
         this.numAccounts = numAccounts;
         this.connectionsList = new HashMap<>();
         for (int i = 0; i < numAccounts; i++) {
-            connectionsList.put(i, new ArrayList<>());
+            connectionsList.put(i, new HashSet<>());
         }
     }
 
-    public void addFriendshipEdge(int origin, int destination)
-    {
-        connectionsList.get(origin).add(destination);
-        connectionsList.get(destination).add(origin);
+    public void addFriendshipEdge(int origin, int destination) {
+        if (origin >= 0 && origin < numAccounts && destination >= 0 && destination < numAccounts) {
+            connectionsList.get(origin).add(destination);
+            connectionsList.get(destination).add(origin);
+        } else {
+            System.out.println("Error! Invalid input ID(s).");
+        }
     }
 
-    public ArrayList<Integer> getFriendsList(int chosenIDNumber) {
-        return connectionsList.getOrDefault(chosenIDNumber, new ArrayList<>());
+    public HashSet<Integer> getFriendsList(int chosenIDNumber) {
+        return connectionsList.getOrDefault(chosenIDNumber, new HashSet<>());
     }
 
     public void printFriendsList(int chosenIDNumber) {
-        ArrayList<Integer> friendsList = getFriendsList(chosenIDNumber);
+        HashSet<Integer> friendsList = getFriendsList(chosenIDNumber);
         if (friendsList.isEmpty()) {
             System.out.println("Error! Inputted ID not found!");
         } else {
@@ -50,36 +53,56 @@ public class ConnectionsGraph {
                 System.out.println("Error! Input ID " + destinationID + " not found!");
             }
             return null; // At least one of the inputs not found
-        } else {
-            HashSet<Integer> setOfVisitedNodes = new HashSet<>();
-            ArrayList<Integer> path = new ArrayList<>();
-            if (findConnectionPathDFS(originID, destinationID, setOfVisitedNodes, path)) {
-                return path;
-            } else {
-                System.out.println("No connection found between ID " + originID + " and ID " + destinationID);
-                return null; // No connection found
+        }
+
+        ArrayList<Integer> queue = new ArrayList<>();
+        HashSet<Integer> setOfVisitedNodes = new HashSet<>();
+        int[] parentArr = new int[numAccounts]; // Array to store the parent of each node
+        queue.add(originID);
+        setOfVisitedNodes.add(originID);
+        boolean isTargetFound = false;
+
+        int queueHead = 0; // Index pointing to the Head of the queue
+
+        while (queueHead < queue.size()) {
+            int currentNode = queue.get(queueHead);
+            queueHead++;
+
+            if (currentNode == destinationID) {
+                isTargetFound = true;
+                break;
             }
-        }
-    }
 
-    private boolean findConnectionPathDFS(int currentNode, int targetNode, HashSet<Integer> setOfVisitedNodes, ArrayList<Integer> path) {
-        if (currentNode == targetNode) {
-            path.add(currentNode);
-            return true; // Found a connection.
-        }
-
-        setOfVisitedNodes.add(currentNode);
-        for (int person : connectionsList.get(currentNode)) {
-            if (!setOfVisitedNodes.contains(person)) {
-                if (findConnectionPathDFS(person, targetNode, setOfVisitedNodes, path)) {
-                    path.add(currentNode); // Add the current node to the beginning of the path
-                    return true;
+            for (int person : connectionsList.get(currentNode)) {
+                if (!setOfVisitedNodes.contains(person)) {
+                    queue.add(person);
+                    setOfVisitedNodes.add(person);
+                    parentArr[person] = currentNode;
                 }
             }
         }
 
-        return false; // No connection found.
+        if (isTargetFound) {
+            return reconstructPath(originID, destinationID, parentArr);
+        } else {
+            System.out.println("No connection found between ID " + originID + " and ID " + destinationID);
+            return null; // No connection found
+        }
     }
+
+    private ArrayList<Integer> reconstructPath(int originID, int destinationID, int[] parentArr) {
+        ArrayList<Integer> path = new ArrayList<>();
+        int currentNode = destinationID;
+
+        while (currentNode != originID) {
+            path.add(0, currentNode); // Add the current node to the beginning of the path
+            currentNode = parentArr[currentNode];
+        }
+
+        path.add(0, originID); // Add the origin node to the beginning of the path
+        return path;
+    }
+
 
 
 
